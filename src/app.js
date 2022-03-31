4e27e6868215a3f5bb74950cbc705ee8533ec31b
iliasanthimos <96637213+iliasanthimos@users.noreply.github.com>
2022-03-31 18:53:47 +0300

    Favicon test

--- src/app.js
+++ src/app.js
@@ -20,6 +20,10 @@ hbs.registerPartials(partialsPath)
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
+app.get('/favicon.ico', (req, res) =>{
+    res.sendStatus(200)
+})
+
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
