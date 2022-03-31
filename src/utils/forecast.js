ce9469ee40937ce61a95224c5cd6796883ce6473
iliasanthimos <96637213+iliasanthimos@users.noreply.github.com>
2022-03-31 20:38:29 +0300

    Add data to about page and provide extra information from the weatherstack api(uv index = wind speed - humidity)

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude,callback) => {
         } else if (body.error) {
             callback('Unable to find location',undefined)
         }else {
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out.' + ' Feels like: ' + body.current.feelslike)
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' Celsius' + ' degress out.' + ' Feels like: ' + body.current.feelslike + ' Celsius degress. ' + ' UV Index: ' + body.current.uv_index + ' Wind Speed: ' + body.current.wind_speed + 'k/h' + ' Humidity: ' + body.current.humidity + '%')
         }
     })
 }
