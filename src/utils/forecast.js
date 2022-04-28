81ef80f4d387179dcf2874c2bdd65b928ea0a6cf
iliasanthimos <96637213+iliasanthimos@users.noreply.github.com>
2022-04-28 23:30:20 +0300

    Update forecast.js
    Weatherstack API Key Refresh

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude,longitude,callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=7abe65817bf3abd4daceea88910d5236&query='+ encodeURIComponent(latitude)+','+ encodeURIComponent(longitude) +'&units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=YOURWEATHERSTACKAPIKEY&query='+ encodeURIComponent(latitude)+','+ encodeURIComponent(longitude) +'&units=m'
 
     request({ url, json: true}, (error,{ body }) => {
         if (error) {
